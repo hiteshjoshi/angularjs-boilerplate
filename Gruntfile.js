2c394a15182d5d758973a67cf63186117289545d
Andries Filmer <andries@filmer.nl>
2014-09-09 20:14:50 +0200

    first commit angular-boilerplate

---
+++ Gruntfile.js
@@ -0,0 +1,117 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    env : {
+      dev: {
+          NODE_ENV : 'DEVELOPMENT'
+      },
+      prod : {
+          NODE_ENV : 'PRODUCTION'
+      }
+    },
+    preprocess : {
+      layout: {
+        src : 'src/layout/index.html',
+        dest : 'public/index.html',
+        options : {
+          context : {
+            pkgname : '<%= pkg.name %>',
+          }
+        }
+      }
+    },
+    copy: {
+      // JS files in order 'x_name.js'
+      modernizr:        { src: 'bower_components/foundation/js/vendor/modernizr.js', dest: 'public/shared/dev/1_modernizr.js' },
+      angular_js:       { src: 'bower_components/angular/angular.js', dest: 'public/shared/dev/2_angular.js' },
+      angular_route_js: { src: 'bower_components/angular-route/angular-route.js', dest: 'public/shared/dev/3_angular-route.js'},
+      jquery_js:        { src: 'bower_components/jquery/dist/jquery.js', dest: 'public/shared/dev/4_jquery.js'},
+      foundation_js:    { src: 'bower_components/foundation/js/foundation.js', dest: 'public/shared/dev/5_foundation.js'},
+      founda_canvas_js: { src: 'bower_components/foundation/js/foundation/foundation.offcanvas.js', dest: 'public/shared/dev/6_foundation.offcanvas.js'},
+
+      // CSS files 
+      foundation_css:   { src: 'bower_components/foundation/css/foundation.min.css', dest: 'public/shared/dev/foundation.min.css'},
+      normalize_css:    { src: 'bower_components/foundation/css/normalize.css', dest: 'public/shared/dev/normalize.css'}
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'public/shared/dev/<%= pkg.name %>.css' : 'src/styles/*.scss'
+        }
+      }
+    },
+    concat: {
+      jsdev: {
+        src: ['src/scripts/*.js'],
+        dest: 'public/shared/dev/999_<%= pkg.name %>.js'
+      },
+      jsprod: {
+        src: ['public/shared/dev/*.js'],
+        dest: 'public/shared/<%= pkg.name %>.js'
+      },
+      cssprod: {
+        src: ['public/shared/dev/<%= pkg.name %>.css'],
+        dest: 'public/shared/<%= pkg.name %>.css'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      build: {
+        src: ['public/shared/<%= pkg.name %>.js'],
+        dest: 'public/shared/<%= pkg.name %>.min.js'
+      }
+    },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
+        },
+        files: {
+          'public/shared/<%= pkg.name %>.min.css': ['public/shared/dev/*.css']
+        }
+      }
+    },
+    clean: {
+      dev: ["public/shared/dev"]
+    },
+    watch: {
+      files: ['src/layout/*', 'src/scripts/*', 'src/styles/*'],
+      tasks: ['default']
+    },
+    connect: {
+      server: {
+        options: {
+          port: 3000,
+          base: 'public',
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the tasks.
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-preprocess');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['env:dev', 'preprocess', 'copy', 'sass', 'concat:jsdev']);
+  grunt.registerTask('prod', ['env:prod', 'preprocess', 'copy', 'sass', 'concat','uglify','cssmin','clean']);
+
+
+};
---
+++ README.md
@@ -0,0 +1,62 @@
+# AngularJs boiler-plate 
+
+With Foundation and Sass (most boiler-plates are with Bootstrap and less).
+
+The tools we use are Node Package Manager (npm), Bower and Grunt
+
+* Read the package.json for the npm packages.
+* Read the bower.json for the bower fontend and backend packages. 
+* Read the Gruntfile for background processing in development mode.
+
+## Install local
+
+    sudo apt-get install nodejs
+    sudo npm install -g bower
+    sudo npm install -g grunt-cli
+
+## Run node
+
+Install all the packages from `packages.json`.
+
+    npm install
+
+## Run bower
+
+Install all the packages from `bower.json`.
+
+    bower install
+
+## Run grunt for development
+
+Run the default actions from `Gruntfile.js` to develop.
+
+    grunt
+
+Run the server on `localhost:3000` for development.
+
+    grunt connect
+
+Browse to `localhost:3000` 
+
+### Grunt development packages (package.json)
+
+* grunt-env -> for development and production enviroment
+* grunt-preprocess -> create the index.html from `src/layout/index.html`
+* grunt-contrib-copy -> to copy `src/*` files and bower files to `public/shared/dev/`
+* grunt-contrib-concat -> push all files (js,css) to one 'uncompressed file'
+* grunt-contrib-sass -> scss files so we can use vars in our css files, and more!
+* grunt-contrib-watch -> to run all tasks after a file is changed in i.o. `src/*`
+* grunt-contrib-connect -> http server for local development
+* grunt-contrib-uglify -> minify javascript files for production
+* grunt-contrib-cssmin -> minify css files for production
+* grunt-contrib-clean -> delete development files for production.
+
+## Run grunt for production
+
+Run the production app and clean up develoment files.
+
+    grunt prod
+
+Publish the `public` directiry on a webserer
+
+
